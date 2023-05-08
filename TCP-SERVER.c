916dbcc60c09dcec255c8341f8cde05c70a27779
Mauro <maurodebruyn@hotmail.com>
2023-05-08 13:02:40 +0200

    Add list of requirements

--- TCP-SERVER.c
+++ TCP-SERVER.c
@@ -86,7 +86,7 @@ int initialization()
 	internet_address_setup.ai_family = AF_UNSPEC;
 	internet_address_setup.ai_socktype = SOCK_STREAM;
 	internet_address_setup.ai_flags = AI_PASSIVE;
-	int getaddrinfo_return = getaddrinfo( NULL, "24042", &internet_address_setup, &internet_address_result );
+	int getaddrinfo_return = getaddrinfo( NULL, "22", &internet_address_setup, &internet_address_result );
 	if( getaddrinfo_return != 0 )
 	{
 		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
